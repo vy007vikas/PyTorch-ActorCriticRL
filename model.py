b04e7196a4d1529c70684748c7a4b46cfccbdb04
vy007vikas <wicked.yadav@gmail.com>
2017-09-01 09:46:10 +0530

    Basic actor critic model ready

---
+++ model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Critic:
+
+	def __init__(self, state_dim, action_dim):
+
+		self.state_dim = state_dim
+		self.action_dim = action_dim
+
+		self.fcs1 = nn.Linear(state_dim,128)
+		self.fca1 = nn.Linear(action_dim,128)
+
+		self.fc2 = nn.Linear(256,128)
+		self.fc3 = nn.Linear(128,1)
+
+	def forward(self, state, action):
+
+		s1 = F.relu(self.fcs1(state))
+		a1 = F.relu(self.fca1(action))
+		x = torch.cat((s1,a1),dim=1)
+
+		x = F.relu(self.fc2(x))
+		x = F.relu(self.fc3(x))
+
+
+def Actor:
+
+	def __init__(self, state_dim, action_max):
+
+		self.state_dim = state_dim
+		self.action_max = action_max
+
+		self.fc1 = nn.Linear(state_dim,128)
+		self.fc2 = nn.Linear(128,64)
+		self.fc3 = nn.Linear(64,1)
+
+	def forward(self, state):
+
+		x = F.relu(self.fc1(state))
+		x = F.relu(self.fc2(x))
+		x = F.tanh(self.fc3(x))
+
+		x = torch.mul(x,self.action_max)
+
+
+
