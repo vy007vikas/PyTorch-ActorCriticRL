d83229f30b4f0db77c877af9b963b9b2d6cdf148
vy007vikas <wicked.yadav@gmail.com>
2017-09-04 10:17:29 +0530

    Updated main flow

--- buffer.py
+++ buffer.py
@@ -19,17 +19,16 @@ class MemoryBuffer:
 		a_arr = np.array([arr[1] for arr in batch])
 		r_arr = np.array([arr[2] for arr in batch])
 		s1_arr = np.array([arr[3] for arr in batch])
-		t_arr = np.array([arr[4] for arr in batch])
 
-		return s_arr, a_arr, r_arr, s1_arr, t_arr
+		return s_arr, a_arr, r_arr, s1_arr
 
 	def len(self):
 		return self.len
 
-	def add(self, s, a, r, s1, t):
-		value = (s,a,r,s1,t)
-		self.buffer.append(value)
+	def add(self, s, a, r, s1):
+		transition = (s,a,r,s1)
 		if self.len < self.maxSize:
 			self.len += 1
 		else :
 			self.buffer.popleft()
+		self.buffer.append(transition)
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import gym
 import numpy as np
 
 import train
+import buffer
 
 env = gym.make('BipedalWalker-v2')
 
 MAX_EPISODES = 100
 MAX_STEPS = 50
+MAX_BUFFER = 256
 S_DIM = env.observation_space.shape
 A_DIM = env.action_space.shape
 A_MAX = env.action_space.high
@@ -16,9 +18,32 @@ print ' Action Dimensions :- ', A_DIM
 print ' Action Max :- ', A_MAX
 
 trainer = train.Trainer(S_DIM, A_DIM, A_MAX)
+ram = buffer.MemoryBuffer(MAX_BUFFER)
 
 for _ep in range(MAX_EPISODES):
-	observation = env.reset()
+	prev_observation = env.reset()
+	new_observation = prev_observation
 	for r in range(MAX_STEPS):
 		env.render()
-		
+		state = new_observation - prev_observation
+
+		# get action based on observation
+		action = trainer.actor.forward(state)
+		observation_ , reward, done, info = env.step(action)
+
+		prev_observation = new_observation
+		new_observation = observation_
+		if done:
+			new_state = None
+		else:
+			new_state = new_observation - prev_observation
+
+		# push this exp in ram
+		ram.add(state, action, reward, new_state)
+
+		# perform optimization
+		trainer.optimize()
+		if done:
+			break
+
+print 'Completed episodes'
