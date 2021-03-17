5e394c6199ba64958fff3c751bf8dd721de95246
i-m-iron-man <50156704+i-m-iron-man@users.noreply.github.com>
2021-03-17 13:49:41 +0000

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -24,13 +24,14 @@ class MemoryBuffer:
 		a_arr = np.float32([arr[1] for arr in batch])
 		r_arr = np.float32([arr[2] for arr in batch])
 		s1_arr = np.float32([arr[3] for arr in batch])
+		done_arr = np.float32([arr[4] for arr in batch])
 
-		return s_arr, a_arr, r_arr, s1_arr
+		return s_arr, a_arr, r_arr, s1_arr, done_arr
 
 	def len(self):
 		return self.len
 
-	def add(self, s, a, r, s1):
+	def add(self, s, a, r, s1, done):
 		"""
 		adds a particular transaction in the memory buffer
 		:param s: current state
@@ -39,7 +40,7 @@ class MemoryBuffer:
 		:param s1: next state
 		:return:
 		"""
-		transition = (s,a,r,s1)
+		transition = (s,a,r,s1,done)
 		self.len += 1
 		if self.len > self.maxSize:
 			self.len = self.maxSize
